caa485429ebb9106fb2fe906e981f241fc5e10d3
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2026-01-04 23:26:35 +0900

    feat: implement user management features including model, controller, views, and styles

--- toy_app/test/application_system_test_case.rb
+++ toy_app/test/application_system_test_case.rb
@@ -1,5 +1,26 @@
 require "test_helper"
+require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # webdrivers gemを使わず、既にインストールされているChromeDriverを直接使用
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    service = Selenium::WebDriver::Service.chrome(
+      executable_path: "/opt/homebrew/bin/chromedriver"
+    )
+    
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options,
+      service: service
+    )
+  end
+  
+  driven_by :headless_chrome, screen_size: [1400, 1400]
 end
